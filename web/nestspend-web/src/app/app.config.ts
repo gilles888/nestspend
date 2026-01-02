a219f02d48b7f9b5772fa703c534363239872f99
Gil Motech <gilmoreau73@gmail.com>
2026-01-02 16:43:09 +0100

    feat: add initial setup for dashboard monthly endpoint and related styles

---
+++ web/nestspend-web/src/app/app.config.ts
@@ -0,0 +1,20 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
+
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimations(),
+    providePrimeNG({
+      theme: { preset: Aura }
+    }),
+  ],
+};
